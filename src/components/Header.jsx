0b0c4f732fa0fb8df24d71537d686d34e938b395
HungTrinhIT <minhhung.it.work@gmail.com>
2024-10-04 19:50:41 +0700

    update app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container-fluid'>
         <Link className='navbar-brand' to='/'>
-          GITHUB FINDER
+          FINDER
         </Link>
         <button
           className='navbar-toggler'
